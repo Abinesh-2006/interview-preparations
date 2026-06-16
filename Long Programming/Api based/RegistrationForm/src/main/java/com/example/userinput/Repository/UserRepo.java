4bb8c13c8af0f9059b5f16c1e6fb34c4884d5922
Priyadharshini-A16 <priyadharshiniprabakaran1@gmail.com>
2026-06-16 11:30:54 +0530

    deleted registrationform

--- Long Programming/Api based/RegistrationForm/src/main/java/com/example/userinput/Repository/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.userinput.Repository;
-
-import com.example.userinput.Model.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<UserModel,Integer> {
-}
